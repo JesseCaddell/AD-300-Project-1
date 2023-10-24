8e17366ee04c4bfd8d08b490dfb3737ec0f6f4bb
Jesse Caddell <80575182+JesseCaddell@users.noreply.github.com>
2023-10-23 20:15:17 -0700

    most recent work

--- src/SongManager.java
+++ src/SongManager.java
@@ -1,5 +1,25 @@
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+import java.io.FileReader;
+import java.io.IOException;
+
 public class SongManager {
 
-    SongManager songManager = new SongManager();
+    public void songReader() {
+        try {
+            CSVReader songReader = new CSVReader(new FileReader("spotify-2023.csv"));
+            for (int i=0; i<1; i++) {
+                for (int j=0; j<1; j++) {
+                    String[] songInfo = songReader.readNext();
+                    System.out.println(songInfo[0]);
+                }
+            }
+        } catch (IOException | CsvValidationException e) {
+            System.out.println("error");
+
+        }
+
+    }
 
 }
